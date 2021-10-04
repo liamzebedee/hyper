bc9289588011e2329aada3b943f77ccf02d93d4e
liamzebedee <liamzebedee@yahoo.com.au>
2021-10-05 01:24:14 +1000

    * add editable text to editor * autoscale images to fit canvas

--- dapp/components/Editor2/index.js
+++ dapp/components/Editor2/index.js
@@ -151,6 +151,164 @@ async function loadIpfsFile(cid, format='json') {
     return content
 }
 
+const EditableText = (props) => {
+    const { stageRef, transformerRef } = props
+    console.log(props)
+    // Hide text node.
+    // Hide transformer (if shown).
+
+    // Create textarea.
+    // Position textarea.
+    // Style textarea.
+    // Handle text input.
+
+    const ref = createRef()
+
+    function handleEdit() {
+        const textNode = ref.current
+        const stage = stageRef.current
+        const tr = transformerRef.current
+
+        // hide text node and transformer:
+        textNode.hide();
+        tr.hide();
+
+        // create textarea over canvas with absolute position
+        // first we need to find position for textarea
+        // how to find it?
+
+        // at first lets find position of text node relative to the stage:
+        const textPosition = textNode.absolutePosition();
+
+        // so position of textarea will be the sum of positions above:
+        const areaPosition = {
+            x: stage.container().offsetLeft + textPosition.x,
+            y: stage.container().offsetTop + textPosition.y,
+        };
+
+        // create textarea and style it
+        const textarea = document.createElement('textarea');
+        document.body.appendChild(textarea);
+
+        // apply many styles to match text on canvas as close as possible
+        // remember that text rendering on canvas and on the textarea can be different
+        // and sometimes it is hard to make it 100% the same. But we will try...
+        textarea.value = textNode.text();
+        textarea.style.position = 'absolute';
+        textarea.style.top = areaPosition.y + 'px';
+        textarea.style.left = areaPosition.x + 'px';
+        textarea.style.width = textNode.width() - textNode.padding() * 2 + 'px';
+        textarea.style.height =
+            textNode.height() - textNode.padding() * 2 + 5 + 'px';
+        textarea.style.fontSize = textNode.fontSize() + 'px';
+        textarea.style.border = 'none';
+        textarea.style.padding = '0px';
+        textarea.style.margin = '0px';
+        textarea.style.overflow = 'hidden';
+        textarea.style.background = 'none';
+        textarea.style.outline = 'none';
+        textarea.style.resize = 'none';
+        textarea.style.lineHeight = textNode.lineHeight();
+        textarea.style.fontFamily = textNode.fontFamily();
+        textarea.style.transformOrigin = 'left top';
+        textarea.style.textAlign = textNode.align();
+        textarea.style.color = textNode.fill();
+        let rotation = textNode.rotation();
+        let transform = '';
+        if (rotation) {
+            transform += 'rotateZ(' + rotation + 'deg)';
+        }
+
+        let px = 0;
+        // also we need to slightly move textarea on firefox
+        // because it jumps a bit
+        let isFirefox =
+            navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+        if (isFirefox) {
+            px += 2 + Math.round(textNode.fontSize() / 20);
+        }
+        transform += 'translateY(-' + px + 'px)';
+
+        textarea.style.transform = transform;
+
+        // reset height
+        textarea.style.height = 'auto';
+        // after browsers resized it we can set actual value
+        textarea.style.height = textarea.scrollHeight + 3 + 'px';
+
+        textarea.focus();
+
+        function removeTextarea() {
+            textarea.parentNode.removeChild(textarea);
+            stage.removeEventListener('click', handleOutsideClick);
+            textNode.show();
+            tr.show();
+            tr.forceUpdate();
+        }
+
+        function setTextareaWidth(newWidth) {
+            if (!newWidth) {
+                // set width for placeholder
+                newWidth = textNode.placeholder.length * textNode.fontSize();
+            }
+            // some extra fixes on different browsers
+            let isSafari = /^((?!chrome|android).)*safari/i.test(
+                navigator.userAgent
+            );
+            let isFirefox =
+                navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+            if (isSafari || isFirefox) {
+                newWidth = Math.ceil(newWidth);
+            }
+
+            let isEdge =
+                document.documentMode || /Edge/.test(navigator.userAgent);
+            if (isEdge) {
+                newWidth += 1;
+            }
+            textarea.style.width = newWidth + 'px';
+        }
+
+        textarea.addEventListener('keydown', function (e) {
+            // hide on enter
+            // but don't hide on shift + enter
+            if (e.keyCode === 13 && !e.shiftKey) {
+                textNode.text(textarea.value);
+                removeTextarea();
+            }
+            // on esc do not set value back to node
+            if (e.keyCode === 27) {
+                removeTextarea();
+            }
+        });
+
+        textarea.addEventListener('keydown', function (e) {
+            const scale = textNode.getAbsoluteScale().x;
+            setTextareaWidth(textNode.width() * scale);
+            textarea.style.height = 'auto';
+            textarea.style.height =
+                textarea.scrollHeight + textNode.fontSize() + 'px';
+        });
+
+        function handleOutsideClick(e) {
+            textNode.text(textarea.value);
+            removeTextarea();
+            if (e.target !== textarea) {
+                
+            }
+        }
+        textarea.addEventListener('blur', handleOutsideClick)
+    }
+
+    return <>
+        <Text
+            ref={ref}
+            draggable
+            onDblClick={handleEdit}
+            {..._.omit(props, ['stageRef', 'transformerRef'])} />
+    </>
+}
+
 const Editor = observer(() => {
     const [editorState] = useState(() => new EditorState())
 
@@ -226,7 +384,7 @@ const Editor = observer(() => {
         const layer = stage.getChildren()[0]
         const allChildren = layer.getChildren()
 
-        const acceptedTypes = ['Image', 'Rect']
+        const acceptedTypes = ['Image', 'Rect', 'Text']
 
         const children = allChildren
             .filter(child => acceptedTypes.includes(child.className));
@@ -295,6 +453,8 @@ const Editor = observer(() => {
             objects
         }
 
+        console.log('file', file)
+
         const { cid } = await ipfs.add(JSON.stringify(file))
 
         console.log('exportCanvasAsHyper', children, objects);
@@ -309,12 +469,15 @@ const Editor = observer(() => {
 
     console.log('refs', refs)
 
+    const transformerRef = useRef()
+
     const objects = editorState.objects.map((object) => {
+
         if (object.type === 'image') {
             const { img } = object
 
             let ref = _.get(refs, object.id)
-            if(!ref) {
+            if (!ref) {
                 ref = createRef()
                 refs[object.id] = ref
             }
@@ -337,13 +500,18 @@ const Editor = observer(() => {
                 key={object.id}
                 draggable
                 {...object.attrs} />
+        } else if (object.type == 'Text') {
+            return <EditableText
+                key={object.id}
+                draggable
+                stageRef={stageRef}
+                transformerRef={transformerRef}
+                {...object.attrs} />
         }
     })
 
+
     console.log('selected', editorState.selected)
-
-    const transformerRef = useRef()
-
     useEffect(() => {
         const selected = editorState.selected.map(id => refs[id])
         console.log(`updateTransformer`, selected)
@@ -363,6 +531,27 @@ const Editor = observer(() => {
         }}>
 
         <h3>hyper editor</h3>
+
+        <button onClick={() => {
+            editorState.addObject({
+                type: 'Text',
+                attrs: {
+                    x: 50,
+                    y: 50,
+                    fontSize: 20,
+                    width: 200,
+                    fontFamily: 'sans-serif',
+                    text: 'LOL WUT'
+                }
+            })
+        }}>
+            <img width={64} height={64} src="/icons/text.svg"/>
+        </button>
+        
+        <button>
+            <img width={64} height={64} src="/icons/image.svg" />
+        </button>
+
         <button onClick={exportCanvas}>Export (.png)</button>
         <button onClick={exportCanvasAsHyper}>Export (.hyper)</button>
 
@@ -380,7 +569,7 @@ const Editor = observer(() => {
                 editorState.setDragActive(true)
                 ev.preventDefault()
             }}
-            onDrop={(ev) => {
+            onDrop={async (ev) => {
                 ev.preventDefault()
                 editorState.setDragActive(false)
 
@@ -390,15 +579,38 @@ const Editor = observer(() => {
                 // we can register it manually:
                 stageRef.current.setPointersPositions(ev);
                 console.log(ev.dataTransfer)
+
                 for (let item of ev.dataTransfer.items) {
                     if (item.type == 'image/png' || item.type == 'image/jpeg') {
                         const reader = new FileReader();
-                        reader.addEventListener('load', () => {
+                        reader.addEventListener('load', async () => {
                             console.log(reader.result)
+
+                            // Scale down image if it is bigger than stage.
+                            const canvas = {
+                                width: stageRef.current.width(),
+                                height: stageRef.current.height()
+                            }
+                            console.log(canvas)
+                            const img = await getImageElement(reader.result)
+                            
+                            const padding = 69 // lel
+                            var hRatio = (canvas.width - padding) / img.width;
+                            var vRatio = (canvas.height - padding) / img.height;
+                            var ratio = Math.min(hRatio, vRatio)
+
                             editorState.addObject({
                                 type: 'image',
                                 url: reader.result,
-                                position: stageRef.current.getPointerPosition()
+                                
+                                attrs: {
+                                    width: img.width * ratio, 
+                                    height: img.height * ratio,
+                                    position: {
+                                        x: padding / 2,
+                                        y: padding / 2
+                                    }
+                                }
                             })
                         }, false)
                         reader.readAsDataURL(item.getAsFile())
@@ -413,12 +625,12 @@ const Editor = observer(() => {
             style={{
                 border: `2px solid ${editorState.dragActive == true ? 'blue' : 'black'}`
             }}>
-            <Stage ref={stageRef} width={700} height={700}>
+            <Stage ref={stageRef} width={700} height={700} onClick={() => editorState.select([]) }>
                 <Layer onClick={() => {
                     editorState.select([])
                 }}>
-                    <Rect x={0} y={0} width={700} height={700} fill="white">
-                    </Rect>                    
+                    {/* <Rect x={0} y={0} width={700} height={700} fill="white">
+                    </Rect>                     */}
                     
                     {objects}
 
